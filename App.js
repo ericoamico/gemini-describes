e6b398ec5ddfe090e4ebc9eaf7ed775e6785cbec
ericoamico <ericorezendeamico@gmail.com>
2024-03-24 22:37:30 -0300

    feat: Add a import off the Image picker

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import * as ImagePicker from 'expo-image-picker';
 export default function App() {
-  return <View accessible></View>;
+  return;
+  <View accessible></View>;
 }
