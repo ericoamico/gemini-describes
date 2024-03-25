40b76b126cd9379e3a2571d0759e9f95aba8c0f1
ericoamico <ericorezendeamico@gmail.com>
2024-03-24 23:21:31 -0300

    set up the Image-Picker

--- App.js
+++ App.js
@@ -2,6 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 export default function App() {
+  const [image, setImage] = useState(null);
+  const pickeImage = async () => {
+    let resoult = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.mediaTypes.All,
+      aspect: [4, 3],
+      quality: 1,
+    })
+    if(!resoult.canceled){
+      setImage(resoult.assets[0].ui);
+    }
+  };
+
+
+  }
+
+
   return;
   <View accessible></View>;
-}
