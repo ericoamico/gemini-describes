0cffd077983fa8e928f2b9d2b7fb2a927d13c067
ericoamico <ericorezendeamico@gmail.com>
2024-03-28 15:52:33 -0300

    Corrections: Spelling and line wrapping

--- App.js
+++ App.js
@@ -1,22 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+
 export default function App() {
   const [image, setImage] = useState(null);
+
   const pickeImage = async () => {
-    let resoult = await ImagePicker.launchImageLibraryAsync({
+    let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.mediaTypes.All,
       aspect: [4, 3],
       quality: 1,
-    })
-    if(!resoult.canceled){
-      setImage(resoult.assets[0].ui);
+    });
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
     }
   };
+}
 
-
-  }
-
-
-  return;
-  <View accessible></View>;
+return;
+<View accessible></View>;
